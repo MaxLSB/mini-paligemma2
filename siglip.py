fe2e571d93b2146de9313cd422bc51010aaa8a5a
MaxLSB <m.lasbordes@gmail.com>
2025-01-26 19:44:29 +0100

    feat: Siglip attention machanism and processing file

--- siglip.py
+++ siglip.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 
 
+################################### Siglip Vision Encoder ###################################
+
+
 class SiglipVisionConfig:
 
     def __init__(
@@ -65,39 +68,109 @@ class SiglipVisionEmbeddings(nn.Module):
         embeddings = embeddings + self.position_embedding(self.position_ids)
         return embeddings
 
-# Need to work on the encoder layer / build the multihead attention layer
+
+class SiglipAttention(nn.Module):
+    """
+    Multi-head Full self-attention mechanism.
+    """
+
+    def __init__(self, config: SiglipVisionConfig):
+        super().__init__()
+        self.config = config
+        self.embed_dim = config.hidden_size
+        self.dropout = config.attention_dropout
+        self.head_dim = self.embed_dim // config.num_attention_heads
+
+        self.q = nn.Linear(self.embed_dim, self.embed_dim)
+        self.k = nn.Linear(self.embed_dim, self.embed_dim)
+        self.v = nn.Linear(self.embed_dim, self.embed_dim)
+        self.out = nn.Linear(self.embed_dim, self.embed_dim)
+
+    def forward(self, x):
+        # [batch_size, num_patches, embed_dim]
+        batch_size, num_patches, _ = x.size()
+        # [batch_size, num_patches, embed_dim] => [batch_size, num_attention_heads, num_patches, head_dim]
+        query = (
+            self.q(x)
+            .view(batch_size, num_patches, self.num_attention_heads, self.head_dim)
+            .transpose(1, 2)
+        )
+        key = (
+            self.k(x)
+            .view(batch_size, num_patches, self.num_attention_heads, self.head_dim)
+            .transpose(1, 2)
+        )
+        values = (
+            self.v(x)
+            .view(batch_size, num_patches, self.num_attention_heads, self.head_dim)
+            .transpose(1, 2)
+        )
+        attn_weights = torch.matmul(query, key.transpose(-2, -1)) / self.head_dim**0.5
+
+        attn_weights = nn.functional.softmax(attn_weights, dim=-1)
+        attn_weights = nn.functional.dropout(
+            attn_weights, p=self.dropout, training=self.training
+        )
+        attn_output = torch.matmul(attn_weights, values)
+        attn_output = attn_output.transpose(
+            1, 2
+        ).contiguous()  # [batch_size, num_patches, num_attention_heads, head_dim]
+        attn_output = attn_output.view(
+            batch_size, num_patches, self.embed_dim
+        )  # [batch_size, num_patches, embed_dim]
+
+        attn_output = self.out(attn_output)
+
+        return attn_output, attn_weights
+
+
 class SiglipEncoderLayer(nn.Module):
     def __init__(self, config: SiglipVisionConfig):
         super().__init__()
         self.config = config
         self.embed_dim = config.hidden_size
-        
+
         self.layer_norm1 = nn.LayerNorm(self.embed_dim, eps=config.layer_norm_eps)
         self.layer_norm2 = nn.LayerNorm(self.embed_dim, eps=config.layer_norm_eps)
         self.dropout = nn.Dropout(config.attention_dropout)
         self.mlp = nn.Sequential(
             nn.Linear(self.embed_dim, config.intermediate_size),
-            nn.ReLU(),
+            nn.GELU(),
             nn.Linear(config.intermediate_size, self.embed_dim),
         )
-        # self.self_attention = 
-
-        pass
+        self.self_attention = SiglipAttention(config)
 
     def forward(self, x):
-        pass
+        # [batch_size, num_patches, embed_dim]
+        residual = x
+        x = self.layer_norm1(x)
+        x, _ = self.self_attention(x)
+        x = residual + x
+
+        residual = x
+        x = self.layer_norm2(x)
+        x = self.mlp(x)
+        x = self.dropout(x)
+        x = residual + x
+        # [batch_size, num_patches, embed_dim]
+        return x
+
 
 class SiglipEncoder(nn.Module):
-    
+
     def __init__(self, config: SiglipVisionConfig):
         super().__init__()
         self.config = config
-        self.layers = nn.ModuleList([SiglipEncoderLayer(config) for _ in range(config.num_hidden_layers)])
+        self.num_layers = config.num_hidden_layers
+        self.layers = nn.ModuleList(
+            [SiglipEncoderLayer(config) for _ in range(self.num_layers)]
+        )
 
     def forward(self, x):
         for layer in self.layers:
             x = layer(x)
         return x
+
 
 class SiglipVisionTransformer(nn.Module):
 
@@ -126,4 +199,5 @@ class SiglipVisionModel(nn.Module):
         self.vision_model = SiglipVisionTransformer(config)
 
     def forward(self, pixel_values):
+        # pixel_values: [batch_size, num_channels, height, width] => [batch_size, num_patches, embed_dim]
         return self.vision_model(pixel_values=pixel_values)
